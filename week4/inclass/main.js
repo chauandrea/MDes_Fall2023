464d407f505ea51cddc5869fbdeee4ff8dab0694
chauandrea <60254288+chauandrea@users.noreply.github.com>
2023-10-03 17:28:20 -0700

    updated images with new names

--- week4/inclass/main.js
+++ week4/inclass/main.js
@@ -1,14 +1,28 @@
+let images = ["c1.jpg", "y1.jpg", "c2.jpg", "y2.jpg"]
+let bigImg = document.getElementById("largeImg")
+let imageIndex = 0
+
 let switchImage = function(event){
-    console.log(event.target)
-    if(!undefined) {
+    //console.log(event.target)
+    if(typeof event === "undefined") {
+        imageIndex = imageIndex + 1
+        if (imageIndex == 4) {
+            imageIndex = 0
+        }
+
+        bigImg.src = images[imageIndex]
 
     }
     else {
-        document.getElementById("largeImg").src=event.target.src
+        bigImg.src=event.target.src
+        imageIndex = parseInt(event.target.id.charAt( 1 )) - 1
+
+        //document.getElementById("largeImg").src=event.target.src
+
     }
 }
 
-let myInterval = window.setInterval(function,2000);
+let myInterval = window.setInterval(switchImage, 2000);
 
 document.getElementById("c1").addEventListener("click", switchImage)
 document.getElementById("y1").addEventListener("click", switchImage)
